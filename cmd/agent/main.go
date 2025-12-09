be306655cf35442cf2b76b39089387945d83bc6b
LemuriiL <Nice.duble@mail.ru>
2025-12-09 03:47:40 +0300

    iter4 V1

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"flag"
+	"log"
 	"time"
 
 	"github.com/LemuriiL/MetricsAllerts/internal/agent"
 )
 
 func main() {
-	agent := agent.NewAgent("http://localhost:8080", 2*time.Second, 10*time.Second)
+	var (
+		serverAddr     string
+		reportInterval int
+		pollInterval   int
+	)
+
+	flag.StringVar(&serverAddr, "a", "http://localhost:8080", "Server address")
+	flag.IntVar(&reportInterval, "r", 10, "Report interval in seconds")
+	flag.IntVar(&pollInterval, "p", 2, "Poll interval in seconds")
+
+	flag.Parse()
+
+	agent := agent.NewAgent(
+		serverAddr,
+		time.Duration(pollInterval)*time.Second,
+		time.Duration(reportInterval)*time.Second,
+	)
+
+	log.Printf("Starting agent, poll=%ds, report=%ds, server=%s", pollInterval, reportInterval, serverAddr)
 	agent.Run()
 }
