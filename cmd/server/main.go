01fa360b0bf9e596ecf5a77a7ce3b94a8a57fbcb
LemuriiL <Nice.duble@mail.ru>
2025-12-09 05:15:53 +0300

    server fix 4

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"log"
-	"strings"
 
 	"github.com/LemuriiL/MetricsAllerts/internal/server"
 	"github.com/LemuriiL/MetricsAllerts/internal/storage"
@@ -13,11 +12,6 @@ func main() {
 	var addr string
 	flag.StringVar(&addr, "a", "localhost:8080", "HTTP server address")
 	flag.Parse()
-
-	if strings.Contains(addr, ":") {
-		parts := strings.Split(addr, ":")
-		addr = ":" + parts[len(parts)-1]
-	}
 
 	store := storage.NewMemStorage()
 	srv := server.New(store)
